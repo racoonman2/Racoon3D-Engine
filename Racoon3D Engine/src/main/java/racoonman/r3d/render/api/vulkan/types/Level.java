9eed4fa0663a73ac86b65a8dc0c80c28d392e645
Ayden <Ayden@wifi-toaster>
2022-08-13 06:50:42 -0700

    commit

---
+++ Racoon3D Engine/src/main/java/racoonman/r3d/render/api/vulkan/types/Level.java
@@ -0,0 +1,20 @@
+package racoonman.r3d.render.api.vulkan.types;
+
+import static org.lwjgl.vulkan.VK10.VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+import static org.lwjgl.vulkan.VK10.VK_COMMAND_BUFFER_LEVEL_SECONDARY;
+
+public enum Level implements IVkType {
+	PRIMARY(VK_COMMAND_BUFFER_LEVEL_PRIMARY),
+	SECONDARY(VK_COMMAND_BUFFER_LEVEL_SECONDARY);
+	
+	private int vkType;
+	
+	private Level(int vkType) {
+		this.vkType = vkType;
+	}
+	
+	@Override
+	public int getVkType() {
+		return this.vkType;
+	}
+}
